5f41de2761eb319ac9ccae2a949d05c4bceb43da
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-20 21:55:44 +0200

    Repsone extended with DateTime
    Repsonse has created date to know when it is outdated, when should it be
    removed

--- src/main/java/com/intland/eurocup/model/Response.java
+++ src/main/java/com/intland/eurocup/model/Response.java
@@ -1,16 +1,27 @@
 package com.intland.eurocup.model;
 
+import org.joda.time.DateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
-@AllArgsConstructor
 @Getter
+@ToString
 public class Response {
 	private ResponseStatus status;
 	private String message;
+	private DateTime createdDate; 
 		
 	public Response() {
 		status = ResponseStatus.NO;
-		message = "";		
+		message = "";
+		createdDate = DateTime.now();
+	}
+	
+	public Response(final ResponseStatus responseStatus, final String message) {
+		status = responseStatus;
+		this.message = message;
+		createdDate = DateTime.now();
 	}
 }
