abba151bd31cf4eadabda49351bc9412e3fdb1ab
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-21 12:17:54 +0200

    GlobalException used ErrorViewModelFactory.
    It handles UknownTerritoryExceptoin.

--- src/main/java/com/intland/eurocup/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/intland/eurocup/controller/exception/GlobalExceptionHandler.java
@@ -4,33 +4,37 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.intland.eurocup.controller.model.ErrorModelViewFactory;
+import com.intland.eurocup.controller.model.ErrorModelViewFactory.ErrorModelViewType;
+import com.intland.eurocup.service.converter.exception.UnkownTerritoryException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-	public static final String DEFAULT_ERROR_VIEW = "error";
-
 	private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 	
-	@ExceptionHandler(value = Exception.class)
+	@Autowired
+	private ErrorModelViewFactory modelViewFactory;
+	
+	
+	@ExceptionHandler(value = UnkownTerritoryException.class)
 	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
 		
-		logger.error("[URL] : {}", req.getRequestURL(), e);
+		logger.error("UnkownTerritoryException catcehd: [URL] : {}", req.getRequestURL(), e);
 		
-		if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null)
-			throw e;
-
-		// Otherwise setup and send the user to a default error-view.
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("exception", e);
-		mav.addObject("url", req.getRequestURL());
-		mav.setViewName(DEFAULT_ERROR_VIEW);
-		return mav;
+		return modelViewFactory.getModelView(ErrorModelViewType.UNKNOWN_TERRITORY);
 	}
 	
+	@ExceptionHandler(value = Throwable.class)
+	public ModelAndView defaultThrowableHandler(HttpServletRequest req, Exception e) throws Exception {
+		
+		logger.error("Exception catcehd: [URL] : {}", req.getRequestURL(), e);
+		
+		return modelViewFactory.getModelView(ErrorModelViewType.OTHER);
+	}
 }
