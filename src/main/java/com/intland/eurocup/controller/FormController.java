a12cc56766d83ba06620f025fb8e6dddc9ef9010
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-22 00:12:49 +0200

    Refactor FormController

--- src/main/java/com/intland/eurocup/controller/FormController.java
+++ src/main/java/com/intland/eurocup/controller/FormController.java
@@ -1,50 +1,57 @@
 package com.intland.eurocup.controller;
 
-import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.intland.eurocup.common.model.Territory;
-import com.intland.eurocup.controller.id.UniqueRequestIdGenerator;
 import com.intland.eurocup.controller.model.ModelViewFactory;
 import com.intland.eurocup.controller.model.ModelViewFactory.ModelViewType;
-import com.intland.eurocup.controller.response.ResponseStorage;
-import com.intland.eurocup.jms.adapter.AdapterSenderService;
-import com.intland.eurocup.model.Response;
-import com.intland.eurocup.model.ResponseStatus;
 import com.intland.eurocup.model.Voucher;
 import com.intland.eurocup.service.converter.TerritoryConverter;
-import com.intland.eurocup.service.converter.exception.UnkownTerritoryException;
 
+import lombok.extern.log4j.Log4j;
+
+/**
+ * Controller for "submit form" handling.
+ */
+@Log4j
 @Controller
 public class FormController {
-
-	private Logger logger = Logger.getLogger(FormController.class.getSimpleName());
-
+	private static final String FORM_URL = "{territory}/form";
+	
 	@Autowired
 	private ModelViewFactory modelViewFactory;
 
+	/**
+	 * Add new custom converter, that converts string to {@link Territory}. 
+	 * @param webdataBinder {@link WebDataBinder}
+	 */
 	@InitBinder
 	public void initBinder(final WebDataBinder webdataBinder) {
 		webdataBinder.registerCustomEditor(Territory.class, new TerritoryConverter());
 	}
 
-	// show add user form
-	@RequestMapping(value = "{territory}/form", method = RequestMethod.GET)
-	public ModelAndView showVoucherForm(@PathVariable Territory territory) {
-		// validate territory
-		logger.info("Territory: " + territory);
-		return modelViewFactory.getModelView(ModelViewType.VOUCHER_FORM_VIEW);
+	/**
+	 * Builds and returns model and view of submit form. 
+	 * @param territory {@link Territory}
+	 * @return
+	 */
+	@GetMapping(value = FORM_URL)
+	public ModelAndView showSubmitForm(@PathVariable Territory territory) {
+		log.info("Show SubmitForm territory: " + territory);
+		return modelViewFactory.getModelView(ModelViewType.VOUCHER_FORM_VIEW, getDefaultVoucher(territory));
+	}
+	
+	private Voucher getDefaultVoucher(final Territory territory) {
+		final Voucher voucher = new Voucher();
+		voucher.setCode("ABCDEF1234");
+		voucher.setEmail("test@gmail.com");
+		voucher.setTerritory(territory);
+		return voucher;
 	}
 }
