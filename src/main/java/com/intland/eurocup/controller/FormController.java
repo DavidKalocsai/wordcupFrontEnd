d78a039b9f2bd175af8356ab6077510c7897d9cd
David Kalocsai <kalocsaidavid@gmail.com>
2019-04-21 12:19:23 +0200

    InitBinder added to FormController to convert Territory Converter

--- src/main/java/com/intland/eurocup/controller/FormController.java
+++ src/main/java/com/intland/eurocup/controller/FormController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,57 +18,33 @@ import org.springframework.web.servlet.ModelAndView;
 import com.intland.eurocup.common.model.Territory;
 import com.intland.eurocup.controller.id.UniqueRequestIdGenerator;
 import com.intland.eurocup.controller.model.ModelViewFactory;
+import com.intland.eurocup.controller.model.ModelViewFactory.ModelViewType;
+import com.intland.eurocup.controller.response.ResponseStorage;
 import com.intland.eurocup.jms.adapter.AdapterSenderService;
+import com.intland.eurocup.model.Response;
+import com.intland.eurocup.model.ResponseStatus;
 import com.intland.eurocup.model.Voucher;
+import com.intland.eurocup.service.converter.TerritoryConverter;
+import com.intland.eurocup.service.converter.exception.UnkownTerritoryException;
 
 @Controller
-public class VoucherController {
+public class FormController {
 
-	private Logger logger = Logger.getLogger(VoucherController.class.getSimpleName());
+	private Logger logger = Logger.getLogger(FormController.class.getSimpleName());
 
-	@Autowired
-	private UniqueRequestIdGenerator uniqueRequestIdGenerator;
-
-	@Autowired
-	private AdapterSenderService senderService;
-	
 	@Autowired
 	private ModelViewFactory modelViewFactory;
 
-	@RequestMapping("/")
-	public String welcome() {
-		return "welcome";
+	@InitBinder
+	public void initBinder(final WebDataBinder webdataBinder) {
+		webdataBinder.registerCustomEditor(Territory.class, new TerritoryConverter());
 	}
 
 	// show add user form
 	@RequestMapping(value = "{territory}/form", method = RequestMethod.GET)
-	public String showVoucherForm(@PathVariable Territory territory, Model model) {
-		model.addAttribute("formData", getDefaultVoucher());
-		model.addAttribute("territory", territory);
-		return "voucherForm";
-	}
-
-	// save or update user
-	@RequestMapping(value = "{territory}/form", method = RequestMethod.POST)
-	public ModelAndView saveOrUpdateUser(@PathVariable Territory territory, @ModelAttribute("formData") @Validated Voucher voucher, BindingResult result) {
-		ModelAndView model; 
-		if (result.hasErrors()) {
-			model = modelViewFactory.getModelView(false);
-		} else {
-			final Long requestId = uniqueRequestIdGenerator.getNext();
-			model = modelViewFactory.getModelView(true);
-			model.addObject("id", requestId);
-			voucher.setTerritory(territory);
-			senderService.send(requestId, voucher);
-			return model;
-		}
-		return model;		
-	}
-	
-	private Voucher getDefaultVoucher() {
-		final Voucher voucher = new Voucher();
-		voucher.setCode("abcdef123");
-		voucher.setEmail("test@gmail.com");
-		return voucher;
+	public ModelAndView showVoucherForm(@PathVariable Territory territory) {
+		// validate territory
+		logger.info("Territory: " + territory);
+		return modelViewFactory.getModelView(ModelViewType.VOUCHER_FORM_VIEW);
 	}
 }
